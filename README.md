4d803908e1e752dcfb3100818dc599d34463464c
Tommy Chheng <tommy@tumblr.com>
2015-09-25 13:58:50 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Soundwich
+
+TBA
---
+++ soundwich/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  soundwich
+//
+//  Created by Tommy Chheng on 9/25/15.
+//  Copyright © 2015 Tommy Chheng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
