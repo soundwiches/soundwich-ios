501ba1adcbac562985d282d16e539a44ff1bb80d
Matt Hayes <mhayes@tumblr.com>
2015-10-21 12:49:48 -0700

    Clean up comments.

--- soundwich/SoundwichPlayerController.swift
+++ soundwich/SoundwichPlayerController.swift
@@ -26,63 +26,6 @@ class SoundwichPlayerController: NSObject, AVAudioPlayerDelegate {
     var currentTime = 0.0
     var totalTime = 0.0
 
-//    /// The player.
-//    var avPlayer:AVAudioPlayer?
-//
-//    func playURL(url:NSURL) {
-//        
-//        do {
-//            try self.avPlayer = AVAudioPlayer(contentsOfURL: url)
-//        } catch {
-//            print("Error creating av audio player")
-//        }
-//        
-//        if avPlayer == nil {
-//            print("Error playing")
-//        }
-//        
-//        avPlayer?.delegate = self
-//        avPlayer?.prepareToPlay()
-//        avPlayer?.volume = 1.0
-//        avPlayer?.play()
-//    }
-//    
-//    /**
-//    Uses AvAudioPlayer to play a sound file.
-//    The player instance needs to be an instance variable. Otherwise it will disappear before playing.
-//    */
-//    func playNSData(data:NSData) {
-//        
-//        do {
-//            try self.avPlayer = AVAudioPlayer(data: data)
-//        } catch {
-//            print("Error creating av audio player")
-//        }
-//        
-//        if avPlayer == nil {
-//            print("Error playing")
-//        }
-//        
-//        avPlayer?.delegate = self
-//        avPlayer?.prepareToPlay()
-//        avPlayer?.volume = 1.0
-//        avPlayer?.play()
-//    }
-//    
-//    func stopAVPLayer() {
-//        if (avPlayer?.playing ?? false) {
-//            avPlayer?.stop()
-//        }
-//    }
-//    
-//    func toggleAVPlayer() {
-//        if (avPlayer?.playing ?? false) {
-//            avPlayer?.pause()
-//        } else {
-//            avPlayer?.play()
-//        }
-//    }
-
     func playAll(soundbites: [Soundbite]) {
         currentTime = 0.0
         totalTime = 0.0
@@ -94,7 +37,6 @@ class SoundwichPlayerController: NSObject, AVAudioPlayerDelegate {
             if let player = try? AVAudioPlayer(contentsOfURL: url!) {
                 player.delegate = self
 
-                print("bite:", bite.start)
                 player.playAtTime(player.deviceCurrentTime + Double(bite.start))
 
                 totalTime = max(totalTime, Double(bite.end))
@@ -172,15 +114,3 @@ class SoundwichPlayerController: NSObject, AVAudioPlayerDelegate {
         // print("audioPlayerDecodeErrorDidOccur:", error)
     }
 }
-
-// MARK: AVAudioPlayerDelegate
-//extension SoundwichPlayerController : AVAudioPlayerDelegate {
-//    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
-//        print("finished playing \(flag)")
-//        delegate?.onFinished()
-//    }
-//    
-//    func audioPlayerDecodeErrorDidOccur(player: AVAudioPlayer, error: NSError?) {
-//        print("\(error!.localizedDescription)")
-//    }
-//}
