2614d2f7d19e6d03fa1bbc52e9257267a1e9a6ea
David Sklar <dfsklar@gmail.com>
2015-10-20 17:11:24 -0700

    soundwich now supports looking for available channels (tracks still empty) and registering a new soundbite

--- soundwich/Soundwich.swift
+++ soundwich/Soundwich.swift
@@ -8,15 +8,63 @@
 
 import Foundation
 
+
+enum SoundwichError: ErrorType {
+    case ChannelAlreadyInUse
+}
+
+    
 class Soundwich {
     var id:String?
     var title:String!
     var audioUrl:String?
     var audioData:NSData?
     var duration:Float?
-    var soundbites:[Soundbite]?
+    var soundbites:[Soundbite]
+    
+    let channelCount = 8
     
     init(title:String) {
         self.title = title
+        self.soundbites = [ ]
     }
+
+    
+    // Returns -1 if no available channel
+    func allAvailableChannels() -> Set<Int> {
+        var availableChannels = Set<Int>()
+        for index in 0...(channelCount-1) {
+            availableChannels.insert(index)
+        }
+        for sb in soundbites {
+            availableChannels.remove(sb.channel)
+        }
+
+        return availableChannels
+    }
+    
+    
+    // Returns -1 if no available channel
+    func nextAvailableChannel() -> Int {
+        let availableChannels = allAvailableChannels()
+        
+        // The only way to get the first item from a Swift Set is to iterate??  Really?
+        for item in availableChannels {
+            return item
+        }
+        
+        return -1
+    }
+
+    
+    // Throws an exception if the new sb's channel is already in use!
+    func addSoundbite(sb : Soundbite) throws {
+        // Check to make sure the new soundbite is using an available channel
+        if self.allAvailableChannels().contains(sb.channel) {
+            soundbites.append(sb)
+        } else {
+            throw SoundwichError.ChannelAlreadyInUse
+        }
+    }
+    
 }
