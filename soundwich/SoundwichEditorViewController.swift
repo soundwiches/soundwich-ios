e7f6043e4c8b03b3e348d1e460f242bb9957940c
Tommy Chheng <tommy@tumblr.com>
2015-10-20 19:20:03 -0700

    Fix #21 gesture navigation conflict

--- soundwich/SoundwichEditorViewController.swift
+++ soundwich/SoundwichEditorViewController.swift
@@ -36,6 +36,11 @@ class SoundwichEditorViewController: UIViewController, AVAudioPlayerDelegate, Me
             action: "onTouchUpInsidePlayPause:",
             forControlEvents: .TouchUpInside
         )
+        
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            self.navigationController?.interactivePopGestureRecognizer?.enabled = false
+        }
     }
 
     override func didReceiveMemoryWarning() {
